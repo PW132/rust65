9d1df83fca1e681a0e858c29d147aadd07d15d06
PW132gl <petercworthington@gmail.com>
2021-11-02 18:09:41 -0700

    command prompt nonsense

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use crate::bus::Segment;
 use crate::cpu::CpuStatus;
 use crate::cpu::execute;
 use std::io::Read;
-use std::io::{Error, ErrorKind};
+use std::io::{Error, ErrorKind, Write};
 use std::fs::File;
 use std::path::Path;
 use std::panic;
@@ -18,23 +18,20 @@ use text_io::read;
 fn main() {
     println!("Starting emulator...");
 
-    let rom_path = Path::new("applesoft-lite-0.4.bin");
-
+    let rom_path = Path::new("applesoft-lite-0.4.bin"); //read ROM file and keep resident in an array
     let mut rom_file = match File::open(rom_path) 
     {
         Err(why) => panic!("couldn't open {}: {}", rom_path.display(), why),
         Ok(file) => file
     };
-
     let mut rom_array: [u8; 0x1fff] = [0; 0x1fff];
     rom_file.read(&mut rom_array);
-
     let rom: &mut[u8] = &mut rom_array[..];
 
     let mut dram_array: [u8; 0xdfff] = [0; 0xdfff]; //reserve 64KB of memory address space
     let dram: &mut[u8] = &mut dram_array[..];
 
-    let memory: &[Segment] = 
+    let memory: &[Segment] = //define memory map
     &[
         Segment {data: dram, start_addr: 0, write_enabled: true, read_enabled: true},
         Segment {data: rom, start_addr: 0xe000, write_enabled: false, read_enabled: true}
@@ -44,6 +41,9 @@ fn main() {
 
     let mut cpu_running: bool = false;
     let mut last_cmd: String; //the command line buffer
+
+    print!("Startup complete! \n>");
+    std::io::stdout().flush().unwrap();
 
     loop
     {
@@ -69,27 +69,23 @@ fn main() {
         {   
             last_cmd = read!("{}\n"); //get text input and store it whole
             
-            if last_cmd.trim() == "run"
+            match last_cmd.trim()
             {
-                cpu_running = true;
-                continue;
+                "run" => cpu_running = true, //run command: start running code
+                "status" => cpu::status_report(&reg), //status command: get status of registers
+                "step" => //step command: run a single operation
+                {   let check: Result<bool, String> = execute(memory, &mut reg);
+                    if check.is_err()
+                    {
+                        println!("{}",check.unwrap_err());
+                    }
+    
+                    cpu::status_report(&reg);},
+                "exit" => break, //exit command: close emulator
+                _ => println!("What?")
             }
-            else if last_cmd.trim() == "step"
-            {
-                let check: Result<bool, String> = execute(memory, &mut reg); //execute an instruction, check for errors
-                if check.is_err()
-                {
-                    println!("{}",check.unwrap_err());
-                }
-
-                cpu::status_report(&reg);
-                continue;
-            }
-            else if last_cmd.trim() == "exit"
-            {
-                break;
-            }
-            println!("What?");
+            print!(">");
+            std::io::stdout().flush().unwrap();
         }
     }
 
